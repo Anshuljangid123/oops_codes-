31877937502771e71ce5539b3a85a03d1f9286f2
Anshul Jangid <anshuljan2003@gmail.com>
2025-06-09 19:03:52 +0530

    Initial Commit

---
+++ hellocpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hellocpp
+//
+//  Created by anshul jangid on 09/06/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
